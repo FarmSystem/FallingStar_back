df91f2ff543abd344fa271f4c97aa9676a8617d5
KwangHwi Jeon <51524745+jeonkwanghwi@users.noreply.github.com>
2022-11-26 19:18:18 +0900

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-n = int(input())
-arr = list((map(int, sys.stdin.readline().strip().split())))
-dp = [0] * n
-
-print("git add complete")
-print("dldkdkddkdkdkkdkddjklfslkdgklsglksgnklsgn")
